3294dcf300ed6bbf440ec91eb06b26c5c06318c3
devendraquickride <devendra@quickride.in>
2025-11-21 09:50:14 +0530

    Initial Commit

---
+++ InspiringQuotes/InspiringQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspiringQuotesApp.swift
+//  InspiringQuotes
+//
+//  Created by S Devendra on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct InspiringQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
